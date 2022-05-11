73ac3ee3a9c1909e6a0d8017a579ccde4ba2710f
Zachary P <zachary.s.prince@outlook.com>
2022-05-11 10:17:44 -0400

    Add author show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
   get '/author', to: 'author#index'
+  get '/author/:id', to: 'author#show'
 end
