cb9970f9b7b505b88c20734ee3b18c7781f42e6f
Zachary P <zachary.s.prince@outlook.com>
2022-05-11 16:50:03 -0400

    Add Book validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :author
+  validates :pages, presence: true
+  validates :name, presence: true
+  validates :genre, presence: true
+  validates :author_id, presence: true
 end
