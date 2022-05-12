3e1cd466e4b96d3f8159d20c20a8172e702191d7
Zachary P <zachary.s.prince@outlook.com>
2022-05-12 15:13:33 -0400

    Add validation tests for book

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe Book do
     it { should belong_to :author }
   end
   describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :fictitious }
+    it { should validate_presence_of :genre }
+    it { should validate_presence_of :author_id }
   end
 end
