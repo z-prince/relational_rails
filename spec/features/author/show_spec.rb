338735e701de19359f4f10ba41401cca7dc93d45
Zachary P <zachary.s.prince@outlook.com>
2022-05-16 07:57:01 -0400

    Add test and logic for seven story

--- spec/features/author/show_spec.rb
+++ spec/features/author/show_spec.rb
@@ -24,4 +24,13 @@ RSpec.describe 'The book show page' do
     save_and_open_page
     expect(page).to have_content(author.name)
   end
+
+  it 'displays the number of books associated with the author' do
+    author = Author.create!(name: 'Fleppy Caliper')
+    book = author.books.create!(name: 'Why do Phones Ring?', pages: 208, genre: 'Philosophy', fictitious: false)
+    book2 = author.books.create!(name: 'Deeds of the Dishwasher', pages: 408, genre: 'Horror', fictitious: true)
+    visit "/books/#{book.id}"
+    save_and_open_page
+    expect(page).to have_content(author.books.count)
+  end
 end
