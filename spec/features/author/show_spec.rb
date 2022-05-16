a03969f23e4dfeafa70715230a54e690e02c3cdc
Zachary P <zachary.s.prince@outlook.com>
2022-05-16 07:53:21 -0400

    Update test paramaters

--- spec/features/author/show_spec.rb
+++ spec/features/author/show_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe 'The song show page' do
+RSpec.describe 'The book show page' do
   # As a user,
-  # when I visit '/songs/1'
-  # I see each song's title and artist
-  # And I do not see any other songs in my database
-  it 'displays the song title' do
-    artist = Artist.create!(name: 'Carly Rae Jepsen')
-    song = artist.songs.create!(title: 'I Really Like You', length: 208, play_count: 123)
-    song2 = artist.songs.create!(title: 'Call Me Maybe', length: 408, play_count: 23)
-    visit "/songs/#{song.id}"
-    # localhost:3000/songs/1
+  # when I visit '/books/1'
+  # I see each book's title and author
+  # And I do not see any other books in my database
+  it 'displays the book title' do
+    author = Author.create!(name: 'Fleppy Caliper')
+    book = author.books.create!(name: 'Why do Phones Ring?', pages: 208, genre: 'Philosophy', fictitious: false)
+    book2 = author.books.create!(name: 'Deeds of the Dishwasher', pages: 408, genre: 'Horror', fictitious: true)
+    visit "/books/#{book.id}"
+    # localhost:3000/books/1
 
-    expect(page).to have_content(song.title)
-    expect(page).to_not have_content(song2.title)
+    expect(page).to have_content(book.title)
+    expect(page).to_not have_content(book2.title)
   end
 
-  it 'displays the name of the artist for the song' do
-    artist = Artist.create!(name: 'Carly Rae Jepsen')
-    song = artist.songs.create!(title: 'I Really Like You', length: 208, play_count: 123)
-    song2 = artist.songs.create!(title: 'Call Me Maybe', length: 408, play_count: 23)
-    visit "/songs/#{song.id}"
+  it 'displays the name of the author for the book' do
+    author = Author.create!(name: 'Fleppy Caliper')
+    book = author.books.create!(name: 'Why do Phones Ring?', pages: 208, genre: 'Philosophy', fictitious: false)
+    book2 = author.books.create!(name: 'Deeds of the Dishwasher', pages: 408, genre: 'Horror', fictitious: true)
+    visit "/books/#{book.id}"
     save_and_open_page
-    expect(page).to have_content(artist.name)
+    expect(page).to have_content(author.name)
   end
 end
