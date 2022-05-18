60294469d3263844e8c8d82b8367c53340166c00
Zachary P <zachary.s.prince@outlook.com>
2022-05-17 20:43:21 -0400

    Add author edit and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/authors', to: 'authors#index'
   get '/authors/new', to: 'authors#new'
   post '/authors', to: 'authors#create'
+  get '/authors/:id/edit', to: 'authors#edit'
+  patch '/authors/:id', to: 'authors#update'
   get '/authors/:id', to: 'authors#show'
   get '/books', to: 'books#index'
   get '/books/:id', to: 'books#show'
