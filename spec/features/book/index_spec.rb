091fc5d90f01d7eef8535c7f5ab812b6651078e6
Zachary P <zachary.s.prince@outlook.com>
2022-05-18 17:57:38 -0400

    Add books true boolean test

--- spec/features/book/index_spec.rb
+++ spec/features/book/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe 'The book show page' do
-  it 'displays the book title' do
+RSpec.describe 'The book index page' do
+  xit 'displays the book title' do
     author = author.create!(name: 'Samantha Moore', living: true)
     book = author.books.create!(name: 'Descencus', pages: 208, genre: 'Thriller', fictitious: true)
     book2 = author.books.create!(title: 'Call Me Maybe', length: 408, play_count: 23)
@@ -12,12 +12,23 @@ RSpec.describe 'The book show page' do
     expect(page).to_not have_content(book2.title)
   end
 
-  it 'displays the name of the author for the book' do
-    author = author.create!(name: 'Carly Rae Jepsen')
-    book = author.books.create!(title: 'I Really Like You', length: 208, play_count: 123)
-    book2 = author.books.create!(title: 'Call Me Maybe', length: 408, play_count: 23)
+  xit 'displays the name of the author for the book' do
+    author = author.create!(name: 'Samantha Moore', living: true)
+    book = author.books.create!(name: 'Descencus', pages: 208, genre: 'Thriller', fictitious: true)
+    book2 = author.books.create!(name: 'Help', pages: 463, genre: 'Philosophy', fictitious: false)
     visit "/books/#{book.id}"
     save_and_open_page
     expect(page).to have_content(author.name)
+  end
+
+  it 'displays the name of the author for the book' do
+    author = author.create!(name: 'Samantha Moore', living: true)
+    book1 = author.books.create!(name: 'Descencus', pages: 208, genre: 'Thriller', fictitious: true)
+    book2 = author.books.create!(name: 'Help', pages: 463, genre: 'Philosophy', fictitious: false)
+
+    visit '/books'
+
+    expect(page).to have_content(book1.name)
+    expect(page).to_not have_content(book2.name)
   end
 end
