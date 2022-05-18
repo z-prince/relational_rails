c5e8bc047cfb70852a5460022c26c4c0d18e784e
Zachary P <zachary.s.prince@outlook.com>
2022-05-18 18:03:17 -0400

    Add if statement to index view

--- spec/features/book/index_spec.rb
+++ spec/features/book/index_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'The book index page' do
   xit 'displays the book title' do
-    author = author.create!(name: 'Samantha Moore', living: true)
-    book = author.books.create!(name: 'Descencus', pages: 208, genre: 'Thriller', fictitious: true)
-    book2 = author.books.create!(title: 'Call Me Maybe', length: 408, play_count: 23)
+    author = Author.create!(name: 'Samantha Moore', living: true, pullitzer_prizes: 123)
+    book1 = Book.create!(name: 'Descencus', pages: 208, genre: 'Thriller', fictitious: true, author_id: author.id)
+    book2 = Book.create!(name: 'Help', pages: 463, genre: 'Philosophy', fictitious: false, author_id: author.id)
     visit "/books/#{book.id}"
     # localhost:3000/books/1
 
@@ -12,19 +12,18 @@ RSpec.describe 'The book index page' do
     expect(page).to_not have_content(book2.title)
   end
 
-  xit 'displays the name of the author for the book' do
-    author = author.create!(name: 'Samantha Moore', living: true)
-    book = author.books.create!(name: 'Descencus', pages: 208, genre: 'Thriller', fictitious: true)
-    book2 = author.books.create!(name: 'Help', pages: 463, genre: 'Philosophy', fictitious: false)
-    visit "/books/#{book.id}"
+  xit 'displays the name of the Author for the book' do
+    author = Author.create!(name: 'Samantha Moore', living: true, pullitzer_prizes: 123)
+    book1 = Book.create!(name: 'Descencus', pages: 208, genre: 'Thriller', fictitious: true, author_id: author.id)
+    book2 = Book.create!(name: 'Help', pages: 463, genre: 'Philosophy', fictitious: false, author_id: author.id)
     save_and_open_page
-    expect(page).to have_content(author.name)
+    expect(page).to have_content(Author.name)
   end
 
-  it 'displays the name of the author for the book' do
-    author = author.create!(name: 'Samantha Moore', living: true)
-    book1 = author.books.create!(name: 'Descencus', pages: 208, genre: 'Thriller', fictitious: true)
-    book2 = author.books.create!(name: 'Help', pages: 463, genre: 'Philosophy', fictitious: false)
+  it 'displays the name of the Author for the book' do
+    author = Author.create!(name: 'Samantha Moore', living: true, pullitzer_prizes: 123)
+    book1 = Book.create!(name: 'Descencus', pages: 208, genre: 'Thriller', fictitious: true, author_id: author.id)
+    book2 = Book.create!(name: 'Help', pages: 463, genre: 'Philosophy', fictitious: false, author_id: author.id)
 
     visit '/books'
 
