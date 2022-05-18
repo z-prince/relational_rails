ca1276fe1ef4af0abaec28b6898ec162d5336316
Zachary P <zachary.s.prince@outlook.com>
2022-05-17 20:34:59 -0400

    Add new author forms

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
   get '/authors', to: 'authors#index'
+  get '/authors/new', to: 'authors#new'
   get '/authors/:id', to: 'authors#show'
   get '/books', to: 'books#index'
   get '/books/:id', to: 'books#show'
   get '/authors/:id/books', to: 'author_books#index'
-  get '/authors/new', to: 'authors#new'
 end
