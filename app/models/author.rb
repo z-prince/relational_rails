77d371561265d5b2a8bf94274945ff21d9f304f3
Zachary P <zachary.s.prince@outlook.com>
2022-05-18 17:23:55 -0400

    Add book edit test

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,5 +2,6 @@ class Author < ApplicationRecord
   has_many :books, dependent: :destroy
   validates :name, presence: true
   validates :living, inclusion: [true, false]
+  validates :pullitzer_prizes, presence: true
   validates_associated :books
 end
