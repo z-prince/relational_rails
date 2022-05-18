e441963e3639ac1b165fca68813572fe36d6fc8a
Zachary P <zachary.s.prince@outlook.com>
2022-05-18 18:54:45 -0400

    Add author update button test

--- spec/features/author/index_spec.rb
+++ spec/features/author/index_spec.rb
@@ -1,1 +1,15 @@
+require 'rails_helper'
 
+RSpec.describe 'The author index page' do
+  it 'has links to update authors' do
+    author1 = Author.create!(name: 'Samantha Moore', living: true, pullitzer_prizes: 123)
+    author2 = Author.create!(name: 'Janis Jalokie', living: true, pullitzer_prizes: 312)
+    author3 = Author.create!(name: 'Galia Kurvinsky', living: false, pullitzer_prizes: 938)
+
+    visit '/authors/'
+
+    click_link('Update Author')
+
+    expect(current_path).to eq("/authors/#{author1.id}/edit")
+  end
+end
